e3296c24c3a0056ccee50d0199b6951610a0ad4b
jphub4128 <67277977+jphub4128@users.noreply.github.com>
2020-07-15 21:43:33 +0530

    Add files via upload

--- RDIP/Codes/sentences.js
+++ RDIP/Codes/sentences.js
@@ -1,3 +1,6 @@
+var comarray= ["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम","राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी","मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम","राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम","बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को","एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब","एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब","John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple","some students like to study in the night","at night some students like to study","John and Mary went to church","Mary and John went to church","John went to church after eating","after eating John went to church","John after eating went to church","did he go to market","he did go to market","the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics", "John goes to the library and studies","John studies and goes to the library", "John ate an apple so did she","she ate an apple so did John", "the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book", "I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her" ];
+
+//shuffling words of the sentence
 function display()
 {
     document.getElementById("d1").innerHTML = "";                                                      
@@ -12,8 +15,7 @@ function display()
     count = 0;
     if (document.getElementById("language").value == "English")
     {
-        answers="";
-        finalsentence="";
+        
         document.getElementById("dis_sen").innerHTML = "Form a sentence (Declarative or Interrogative or any other type) from the given words";
         document.getElementById("dis_sub").innerHTML = "(select the buttons in proper order)";
 
@@ -108,7 +110,9 @@ function newsentence(k)
         count++;
         var len = document.getElementById("created_sentence").value;
         if (count === len) {
-        document.getElementById("check").innerHTML ="<center><button id='correctness'  onclick='compares()'>Check the correctness</button></center>"
+        document.getElementById("check").innerHTML ="<center><button >Check the correctness</button></center>"
+        document.getElementById("getthecorrectsen").innerHTML = "<center><button id='showansbtn' onclick='gets()'>Get Correct Sentence</button></center>";
+    
     }
     }
 function reformsen() 
@@ -119,6 +123,7 @@ function reformsen()
         document.getElementById("bee").innerHTML = " ";
         document.getElementById("reform").innerHTML = " ";
         document.getElementById("check").innerHTML = "";
+        document.getElementById("getthecorrectsen").innerHTML = "";
         for (var i = 0; i < 10; i++)
         {
             var a = document.getElementById(i).value
@@ -128,21 +133,23 @@ function reformsen()
         }
     
     } 
-    var answers="";
-    function compares(){
-
-        var str= document.getElementById("created_sentence").value;
-        str2=str.trim();
+    
+ function compares(){
+        var form=document.getElementById("bee").value;
+        var str= form.trim();
     for(let i=0;i<comarray.length;i++){
         var str1 = comarray[i];
-         console.log(i, str1.localeCompare(str2), str2, str2.length, str1, str1.length)
-        var n = str1.localeCompare(str2);
+         console.log(i, str1.localeCompare(str), str, str.length, str1, str1.length)
+        var n = str1.localeCompare(str);
         if (n == 0) {
-            document.getElementById("d3").innerHTML = "RIGHT";
-
+            document.getElementById("rightanswer").innerHTML = "RIGHT ANSWER!!!";
             return;
         }
+       
+        
     }
-    document.getElementById("d4").innerHTML = "WRONG";
-
+    document.getElementById("wronganswer").innerHTML = "WRONG ANSWER!!!";
+    document.getElementById("getthecorrectsen").innerHTML = "<center><button id='showansbtn' onclick='gets()'>Get Correct Sentence</button></center>";
+    
+    
 }
